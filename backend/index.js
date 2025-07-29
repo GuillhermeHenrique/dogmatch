e86d69442264c170438355811eef4c0560aafe8f
GuillhermeHenrique <piresguilherme003@gmail.com>
2025-07-29 17:15:30 -0300

    chore: perform final tweaks and complete RESTful API

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 // solve CORS
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
 
 // public folder for images
 app.use(express.static("public"));
