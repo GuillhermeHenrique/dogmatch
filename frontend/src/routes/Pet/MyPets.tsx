0dfed3ff6b83ff2537f65ea438cf3ad14afb6111
GuillhermeHenrique <piresguilherme003@gmail.com>
2025-08-11 19:40:32 -0300

    feat: implement pet deletion functionality

--- frontend/src/routes/Pet/MyPets.tsx
+++ frontend/src/routes/Pet/MyPets.tsx
@@ -8,23 +8,59 @@ import api from "../../utils/api";
 
 import RoundedImage from "../../components/layout/RoundedImage";
 
+import type { AxiosError } from "axios";
+
+import { useFlashMessage } from "../../hooks/useFlashMessage";
+
 import type { Pet } from "../../types/Pet";
 
 const MyPets = () => {
   const [pets, setPets] = useState<Pet[]>([]);
   const [token] = useState(localStorage.getItem("token") || "");
+  const { setFlashMessage } = useFlashMessage();
 
   useEffect(() => {
-    api
-      .get("/pets/mypets", {
+    const showPets = async () => {
+      try {
+        const response = await api.get("/pets/mypets", {
+          headers: {
+            Authorization: `Bearer ${JSON.parse(token)}`,
+          },
+        });
+
+        setPets(response.data.pets);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    showPets();
+  }, [token]);
+
+  const removePet = async (id: string) => {
+    try {
+      const response = await api.delete(`/pets/${id}`, {
         headers: {
           Authorization: `Bearer ${JSON.parse(token)}`,
         },
-      })
-      .then((response) => {
-        setPets(response.data.pets);
       });
-  }, [token]);
+
+      const updatedPets = pets.filter((pet) => pet.id != id);
+
+      setPets(updatedPets);
+
+      setFlashMessage("Pet removed successfully!", "success");
+
+      return response;
+    } catch (error) {
+      const err = error as AxiosError<{ message: string }>;
+
+      const msgText =
+        err.response?.data?.message || "An unexpected error occurred!";
+
+      setFlashMessage(msgText, "error");
+    }
+  };
 
   return (
     <>
@@ -62,7 +98,10 @@ const MyPets = () => {
                     >
                       Edit
                     </Link>
-                    <button className={`${classes.btn} ${classes.btn_remove}`}>
+                    <button
+                      onClick={() => removePet(pet.id)}
+                      className={`${classes.btn} ${classes.btn_remove}`}
+                    >
                       Remove
                     </button>
                   </>
